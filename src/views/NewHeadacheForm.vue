5ab8e6aa352c594b8cd390ab3cdc51c2805fea99
Alessia <miccoli.alessia@hotmail.it>
2020-03-06 15:51:26 +0100

    added medicin-list form container

--- src/views/NewHeadacheForm.vue
+++ src/views/NewHeadacheForm.vue
@@ -13,10 +13,28 @@
         required
       ></v-text-field>
       <h3>Select Headache Intensity:</h3>
-      <div class="d-flex align-center justify-space-around">
+      <div class="d-flex align-center justify-space-between btns-container">
         <v-btn color="primary">Light</v-btn>
         <v-btn color="primary">Medium</v-btn>
         <v-btn color="primary">Strong</v-btn>
+      </div>
+      <h3>Medicine(s) taken:</h3>
+      <div class="medicine-container">
+        <div>
+          <v-text-field
+            v-model="medicineName"
+            label="Medicine name"
+            required
+          ></v-text-field>
+          <h4>Was it effective?</h4>
+          <div class="d-flex align-center justify-space-around btns-container">
+            <v-btn color="primary">Yes</v-btn>
+            <v-btn color="primary">No</v-btn>
+          </div>
+        </div>
+        <div class="d-flex align-items-center justify-content-center btns-container">
+          <v-btn class="w-100" color="primary"><v-icon>mdi-plus</v-icon></v-btn>
+        </div>
       </div>
     </div>
   </v-content>
@@ -28,6 +46,7 @@ export default {
   data: ()=> ({
     startDate: '',
     endDate: '',
+    medicineName: ''
   })
 }
 </script>
@@ -37,7 +56,23 @@ export default {
     padding: 20px;
   }
 
-  h3{
+  h3, h4{
+    margin-bottom: 20px;
+  }
+
+  .w-100{
+    width: 100%;
+    margin-top: 20px;
+  }
+
+  .medicine-container{
+    border: 1px solid #5c6bc06e;
+    padding: 20px;
+    margin-top: 20px;
+    border-radius: 5px;
+  }
+
+  .btns-container{
     margin-bottom: 20px;
   }
 
